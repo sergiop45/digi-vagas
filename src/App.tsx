6beee08162c4f64cedfeb2556b57ddb9c9bd4c26
sergiop45 <sergiopsantos33@gmail.com>
2022-10-29 16:12:04 -0300

    config-inicial-e-react-router-dom

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h2>CRUD de vagas</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+
+        <Route path='/home' element={<p>Pagina Home</p>} />
+        <Route path='/usuarios' element={<p>Pagina Usuarios</p>} />
+        <Route path='/vagas' element={<p>Pagina Vagas</p>} />
+        <Route path='*' element={<Navigate to='/home' />} />
+
+    </Routes>
+  )
+}
+
+export default AppRoutes
